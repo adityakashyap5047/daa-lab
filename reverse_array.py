4c4b585e722b3e997dad2c6697fcba27240d7c73
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-02-06 10:16:36 +0530

    reverse an array

--- reverse_array.py
+++ reverse_array.py
@@ -2,6 +2,8 @@ numbers = [1, 2, 3, 4, 5, 6]
 
 #reverse an array
 for i in range(len(numbers)//2):
-        numbers[i], numbers[len(numbers)-i-1] = numbers[len(numbers)-i-1], numbers[i]
+        numbers[i] = numbers[i] + numbers[len(numbers)-i-1]
+        numbers[len(numbers)-i-1] = numbers[i] - numbers[len(numbers)-i-1]
+        numbers[i] = numbers[i] - numbers[len(numbers)-i-1]
 
 print(numbers)
