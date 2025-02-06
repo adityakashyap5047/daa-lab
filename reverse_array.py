3aa28b45b59a3a79737d411635e4817f540149e6
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-02-06 10:24:19 +0530

    swapping of 2 numbers withour 3 variable

--- reverse_array.py
+++ reverse_array.py
@@ -2,8 +2,14 @@ numbers = [1, 2, 3, 4, 5, 6]
 
 #reverse an array
 for i in range(len(numbers)//2):
-        numbers[i] = numbers[i] + numbers[len(numbers)-i-1]
-        numbers[len(numbers)-i-1] = numbers[i] - numbers[len(numbers)-i-1]
-        numbers[i] = numbers[i] - numbers[len(numbers)-i-1]
+    #Swap of 2 numbers without third variable using arithmetic operation
+    numbers[i] = numbers[i] + numbers[len(numbers)-i-1]
+    numbers[len(numbers)-i-1] = numbers[i] - numbers[len(numbers)-i-1]
+    numbers[i] = numbers[i] - numbers[len(numbers)-i-1]
+
+    #Swap of 2 numbers without third variable using XOR ( ^ )
+    # numbers[i] = numbers[i] ^ numbers[len(numbers)-i-1]
+    # numbers[len(numbers)-i-1] = numbers[i] ^ numbers[len(numbers)-i-1]
+    # numbers[i] = numbers[i] ^ numbers[len(numbers)-i-1]
 
 print(numbers)
