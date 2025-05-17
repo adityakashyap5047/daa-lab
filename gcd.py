77861428df3332fc61598437a2f84deb9d4bf893
Aditya Kashyap <adityakashyap5047@gmail.com>
2025-05-17 10:07:15 +0530

    finding the gcd without using python library

--- gcd.py
+++ gcd.py
@@ -3,3 +3,13 @@ import math
 a = int(input("Enter first number: "))
 b = int(input("Enter second number: "))
 print("GCD:", math.gcd(a, b))
+
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+lcm = abs(a * b) // gcd(a, b)
+print("LCM:", lcm)
